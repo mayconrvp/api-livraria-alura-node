ce4e8eac61ba4f1a4fac2ec9b65c311cab776f5f
Maycon <maycon.rvp@outlook.com>
2022-04-21 22:42:11 -0300

    Associado entidades autor e livros utilizando populate e adicionado metodo de listar livro por editora

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -3,7 +3,9 @@ import livros from "../models/Livro.js";
 class LivroController {
   
   static listarLivros = (req, res) => {
-    livros.find((err, livros) => {
+    livros.find()
+      .populate('autor')
+      .exec((err, livros) => {
       res.status(200).json(livros);
     });
   }
@@ -34,7 +36,9 @@ class LivroController {
 
   static listarLivroPorId = (req, res) => {
     const id = req.params.id;
-    livros.findById(id, (err, livros) => {
+    livros.findById(id)
+    .populate('autor', 'nome')
+    .exec((err, livros) => {
       if(err){
         res.status(400).send({message: `${err.message} - Id do livro não localizado`});
       }else{
@@ -52,7 +56,18 @@ class LivroController {
         res.status(500).send({message: err.message})
       }
     })
+  }
 
+  static listarLivroPorEditora = (req, res) => {
+    const editora = req.query.editora;
+
+    livros.find({'editora': editora}, {}, (err, livros) => {
+      if(err){
+        res.status(400).send({message: `${err.message} - Não foram localizados livros com a editora informada`});
+      }else{
+        res.status(200).send(livros);
+      }
+    })
   }
 
 }
